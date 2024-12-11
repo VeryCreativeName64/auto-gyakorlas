738ab26056b31ff3e017b0d50687e731d410c14b
marekbendeguz@gmail.com <marekbendeguz@gmail.com>
2024-12-11 12:06:33 +0100

    Ezt a téli szünetben gyakorolnigit add .git add .

---
+++ bevezetes.py
@@ -0,0 +1,8 @@
+def auto_adat():
+    auto=input("Add meg az auto nevét: ")
+    gyarev=input("Add meg az auto gyártási évét: ")
+
+    print(f"Autó neve: {auto}")
+    print(f"Gyártási dátuma: {gyarev}")
+
+    
